38ee63500da61be78c276ae62b7e60bcc9714bd0
JangDongHa <jdh3340@naver.com>
2022-10-26 16:22:42 +0900

    Test: Generate key API

--- src/test/java/com/example/mainkeyserver/service/KeyServiceTest.java
+++ src/test/java/com/example/mainkeyserver/service/KeyServiceTest.java
@@ -1,10 +1,26 @@
 package com.example.mainkeyserver.service;
 
+import com.example.mainkeyserver.util.KeyGenerator;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 public class KeyServiceTest {
+    @Autowired
+    private KeyGenerator keyGenerator;
 
+    // Key 생성 API
+    @Test
+    void generateKey(){
+        // Given
+        int length = 16;
+        // When
+        String newKey = keyGenerator.generateKey();
+        // Then
+        assertNotNull(newKey);
+        assertEquals(16, newKey.length());
+    }
 }
